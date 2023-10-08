03a3765e909f18529f6d32815ce5f593283264b3
leiber <leiberbertel777@gmail.com>
2023-10-08 18:29:47 -0500

    initial commit

---
+++ src/main/java/com/leiber/pizza/LeiberPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.leiber.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiberPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeiberPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leiber/pizza/LeiberPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leiber.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeiberPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
