0158c494c1ae35d7e712f2600004f466886d48f6
leiber <leiberbertel777@gmail.com>
2023-10-13 20:47:56 -0500

    perf: add new services for crud pizzas

--- src/main/java/com/leiber/pizza/services/PizzaServices.java
+++ src/main/java/com/leiber/pizza/services/PizzaServices.java
@@ -1,12 +1,13 @@
 package com.leiber.pizza.services;
+import com.leiber.pizza.errors.PizzaAlreadyExistsException;
 import com.leiber.pizza.errors.PizzaNotFoundException;
+import com.leiber.pizza.errors.PizzaWithIdNullException;
 import com.leiber.pizza.persistence.entity.Pizza;
 import com.leiber.pizza.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PizzaServices {
@@ -28,5 +29,36 @@ public class PizzaServices {
     public Pizza getPizzaById(int id) {
         return this.pizzaRepository.findById(id)
                 .orElseThrow(() -> new PizzaNotFoundException(id));
+    }
+
+    public Pizza save(Pizza pizza) {
+        if (pizza == null || pizza.getIdPizza() == null) {
+            throw new PizzaWithIdNullException();
+        }
+
+        if(this.pizzaRepository.existsById(pizza.getIdPizza())) {
+            throw new PizzaAlreadyExistsException(pizza);
+        }
+
+        return this.pizzaRepository.save(pizza);
+    }
+
+
+    public Pizza update(Pizza pizza) {
+        if (pizza == null || pizza.getIdPizza() == null) {
+            throw new PizzaWithIdNullException();
+        }
+        if (!this.pizzaRepository.existsById(pizza.getIdPizza())) {
+            throw new PizzaNotFoundException(pizza.getIdPizza());
+        }
+
+        return this.pizzaRepository.save(pizza);
+    }
+
+    public void delete(int id) {
+        if (!this.pizzaRepository.existsById(id)){
+            throw new PizzaNotFoundException(id);
+        }
+        this.pizzaRepository.deleteById(id);
     }
 }
