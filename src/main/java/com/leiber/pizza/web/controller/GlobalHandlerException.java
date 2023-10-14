a5f527794814ac8f4bab7790d79b57c8b87b8618
leiber <leiberbertel777@gmail.com>
2023-10-13 20:49:25 -0500

    perf: add class global handler errors

--- src/main/java/com/leiber/pizza/web/controller/GlobalHandlerException.java
+++ src/main/java/com/leiber/pizza/web/controller/GlobalHandlerException.java
@@ -1,6 +1,8 @@
 package com.leiber.pizza.web.controller;
 
+import com.leiber.pizza.errors.PizzaAlreadyExistsException;
 import com.leiber.pizza.errors.PizzaNotFoundException;
+import com.leiber.pizza.errors.PizzaWithIdNullException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +12,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalHandlerException {
 
     @ExceptionHandler(PizzaNotFoundException.class)
-    public ResponseEntity<String> handlePizzaNotFound(PizzaNotFoundException e) {
+    public ResponseEntity<String> handlerPizzaNotFound(PizzaNotFoundException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PizzaAlreadyExistsException.class)
+    public ResponseEntity<String> handlerPizzaAlreadyExists(PizzaAlreadyExistsException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(PizzaWithIdNullException.class)
+    public ResponseEntity<String> handlerPizzaWithIdNull(PizzaWithIdNullException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
