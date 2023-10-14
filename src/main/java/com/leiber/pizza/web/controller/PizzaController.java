d0cd8fef274252687afe4afacf1b751bf5c6907f
leiber <leiberbertel777@gmail.com>
2023-10-13 20:48:51 -0500

    feat: add controller delete, save, update

--- src/main/java/com/leiber/pizza/web/controller/PizzaController.java
+++ src/main/java/com/leiber/pizza/web/controller/PizzaController.java
@@ -6,10 +6,7 @@ import com.leiber.pizza.services.PizzaServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,4 +36,21 @@ public class PizzaController {
         Pizza pizza = this.pizzaServices.getPizzaById(id);
         return ResponseEntity.ok(pizza);
     }
+
+    @PostMapping
+    public ResponseEntity<Pizza> add(@RequestBody Pizza pizza) {
+        return ResponseEntity.ok(this.pizzaServices.save(pizza));
+    }
+
+    @PutMapping
+    public ResponseEntity<Pizza> update(@RequestBody Pizza pizza) {
+        return ResponseEntity.ok(this.pizzaServices.update(pizza));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        this.pizzaServices.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
