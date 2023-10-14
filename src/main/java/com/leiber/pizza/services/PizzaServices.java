1c26d9e888fb1f6ae90df551330617e3267d7fbe
leiber <leiberbertel777@gmail.com>
2023-10-14 00:01:07 -0500

    feat: add new method for entity Pizza

--- src/main/java/com/leiber/pizza/services/PizzaServices.java
+++ src/main/java/com/leiber/pizza/services/PizzaServices.java
@@ -6,6 +6,7 @@ import com.leiber.pizza.persistence.entity.Pizza;
 import com.leiber.pizza.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,8 +23,34 @@ public class PizzaServices {
         return this.pizzaRepository.findAll();
     }
 
-    public List<Pizza> getAllUnavailable() {
-        return this.pizzaRepository.findByAvailableFalse();
+    public List<Pizza> getUnavailable() {
+        return this.pizzaRepository.findAllByAvailableFalse();
+    }
+
+    public List<Pizza> getAvailable(){
+        return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
+    }
+
+    public Pizza getByName(String name){
+        return this.pizzaRepository.findFirstByAvailableTrueAndNameIgnoreCase(name)
+                .orElseThrow(() -> new RuntimeException("No existe la pizza con el nombre: " + name));
+    }
+
+    public List<Pizza> getCheapest(@PathVariable Double price){
+        return this.pizzaRepository.findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(price);
+    }
+
+
+    public List<Pizza> getWith(String ingredient){
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionContainingIgnoreCase(ingredient);
+    }
+
+    public List<Pizza> getWithOut(String ingredient){
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(ingredient);
+    }
+
+    public int countVeganPizza() {
+        return this.pizzaRepository.countByVeganTrue();
     }
 
     public Pizza getPizzaById(int id) {
