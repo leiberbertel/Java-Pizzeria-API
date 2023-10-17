04bba34354077feb1e9ef4aaf078095ead87d403
leiber <leiberbertel777@gmail.com>
2023-10-17 12:10:15 -0500

    refactor: change name entities

--- src/main/java/com/leiber/pizza/persistence/audit/AuditPizzaListener.java
+++ src/main/java/com/leiber/pizza/persistence/audit/AuditPizzaListener.java
@@ -1,6 +1,6 @@
 package com.leiber.pizza.persistence.audit;
 
-import com.leiber.pizza.persistence.entity.Pizza;
+import com.leiber.pizza.persistence.entity.PizzaEntity;
 import jakarta.persistence.PostLoad;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostUpdate;
@@ -8,24 +8,24 @@ import jakarta.persistence.PreRemove;
 import org.springframework.util.SerializationUtils;
 
 public class AuditPizzaListener {
-    private Pizza currentValue;
+    private PizzaEntity currentValue;
 
     @PostLoad
-    public void postLoad(Pizza entity) {
+    public void postLoad(PizzaEntity entity) {
         System.out.println("POST LOAD");
         this.currentValue = SerializationUtils.clone(entity);
     }
 
     @PostPersist
     @PostUpdate
-    public void OnPostPersist(Pizza entity) {
+    public void OnPostPersist(PizzaEntity entity) {
         System.out.println("POST PERSIST OR UPDATE");
         System.out.println("OLD VALUE:" + this.currentValue.toString());
         System.out.println("NEW VALUE:" + entity.toString());
     }
 
     @PreRemove
-    public void OnPreDelete(Pizza entity) {
+    public void OnPreDelete(PizzaEntity entity) {
         System.out.println(entity.toString());
     }
 }
