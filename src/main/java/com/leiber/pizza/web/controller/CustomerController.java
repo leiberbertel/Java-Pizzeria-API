c648007da45bce7f9f8e84efcb442b294d16fae1
leiber bertel <leiberbertel777@gmail.com>
2023-11-03 20:12:10 -0500

    refactor: update constructor Customer

--- src/main/java/com/leiber/pizza/web/controller/CustomerController.java
+++ src/main/java/com/leiber/pizza/web/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.leiber.pizza.web.controller;
 
 import com.leiber.pizza.persistence.entity.CustomerEntity;
+import com.leiber.pizza.persistence.entity.OrderEntity;
 import com.leiber.pizza.services.CustomerService;
+import com.leiber.pizza.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +11,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/customers")
 public class CustomerController {
     private final CustomerService customerService;
+    private final OrderService orderService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, OrderService orderService) {
         this.customerService = customerService;
+        this.orderService = orderService;
     }
 
     @GetMapping("/phone/{phone}")
@@ -24,4 +30,11 @@ public class CustomerController {
         CustomerEntity customerEntity = this.customerService.findByPhone(phone);
         return ResponseEntity.ok(customerEntity);
     }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<List<OrderEntity>> getCustomerOrders(@PathVariable String id) {
+        List<OrderEntity> orderEntities = this.orderService.getCustomerOrder(id);
+        return ResponseEntity.ok(orderEntities);
+    }
+
 }
